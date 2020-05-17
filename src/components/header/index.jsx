89414ffd2d4cb7452519cdc04cd0c44c45574742
Marcio Filho <marcio_filho02@yahoo.com.br>
2020-05-17 03:39:16 -0300

    Primeira aula

---
+++ src/components/header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./Header.css"
+
+export default function Header (props){
+    return(
+        <div className="Header" >
+        <div className="Logo">Puck Filmes</div>
+        <input type="text" name="search" id="search"></input>
+        </div>
+    )
+
+}
