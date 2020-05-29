870b86ef317b17c2370ff9730db40d451f425163
Marcio Filho <marcio_filho02@yahoo.com.br>
2020-05-29 20:38:20 -0300

    Implementando Card

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './App.css';
 
 import Header from "./components/header"
+import Card from "./components/card/index"
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <div className="content">
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+     </div>
     </div>
   );
 }
---
+++ src/components/card/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './Card.css'
+
+export default function Card(){
+    return(
+        <div className="card-filmes">
+            <div>
+                <span className="material-icons fav-btn">favorite_border</span>
+                <div className="poster-image"></div>
+                <div className="especificacao">
+                    <h4>Vingadores: guerra infinita</h4>
+                    <span className="material-icons">star</span>
+                    9.8
+                    </div>
+                <a href="#" className="btn-detail">
+                <span className="material-icons">add_circle_outline</span> 
+                 detalhes
+                </a>
+            </div>
+        </div>
+    )
+
+}
+    
