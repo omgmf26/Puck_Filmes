db18e5495bf093279a4721eba556ca12f0f4ae62
Marcio Filho <marcio_filho02@yahoo.com.br>
2020-06-04 15:00:04 -0300

    Conclusao primeira entrega

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 import Header from "./components/header"
 import Card from "./components/card/index"
+import Botons from "./components/botons"
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <div className="content">
+       <Botons/>
+     </div>
      <div className="content">
       <Card/>
       <Card/>
