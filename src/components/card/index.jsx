1d822c68c457bee1e67d35ae786d939983e8e1df
Marcio Filho <marcio_filho02@yahoo.com.br>
2020-07-02 19:54:22 -0300

    commit com erros

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -3,16 +3,21 @@ import React from 'react'
 
 import './Card.css'
 
-export default function Card(){
+export default function Card({filme}){
+const imgUrl = "https://image.tmdb.org/t/p/w1280/"
     return(
         <div className="card-filmes">
             <div>
                 <span className="material-icons fav-btn">favorite_border</span>
-                <div className="poster-image"></div>
+                <div 
+                className="poster-image" 
+                style={{ backgroundImage: `url( ${imgUrl} ${filme.poster_path} )` }}
+                >
+                </div>
                 <div className="especificacao">
-                    <h4>Vingadores: guerra infinita</h4>
+                    <h4>{filme.title}</h4>
                     <span className="material-icons">star</span>
-                    9.8
+                    {filme.vote_average}
                     </div>
                 <a href="#" className="btn-detail">
                 <span className="material-icons">add_circle_outline</span> 
