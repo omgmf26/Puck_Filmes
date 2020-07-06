d35c633165ada09b6311d84d5228d760080e6579
Nathalia Silva <nathaliasilvanov@gmail.com>
2020-07-06 14:51:15 -0300

    api implementada

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import './Card.css'
 
 export default function Card({filme}){
+const nota = filme.vote_average.toFixed(1)
 const imgUrl = "https://image.tmdb.org/t/p/w1280/"
     return(
         <div className="card-filmes">
@@ -17,7 +18,7 @@ const imgUrl = "https://image.tmdb.org/t/p/w1280/"
                 <div className="especificacao">
                     <h4>{filme.title}</h4>
                     <span className="material-icons">star</span>
-                    {filme.vote_average}
+                    {nota}
                     </div>
                 <a href="#" className="btn-detail">
                 <span className="material-icons">add_circle_outline</span> 
